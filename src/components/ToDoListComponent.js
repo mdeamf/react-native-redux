469b79e9a0174ba5756779e68a62dbc096d99fa2
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-14 14:38:06 -0300

    feat: implementing to do dispatching and logging

--- src/components/ToDoListComponent.js
+++ src/components/ToDoListComponent.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { FlatList, View, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { FlatList, View, Text, Button } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD_TO_DO, REMOVE_TO_DO } from '../store/actions';
+
+const LOG = '[TO DO LIST COMPONENT]';
 
 const ToDoItem = ({ item }) => {
+  console.log(LOG, 'list item', item);
   return (
     <View>
       <Text>ID: {item.id}</Text>
@@ -14,10 +18,40 @@ const ToDoItem = ({ item }) => {
 export const ToDoListComponent = () => {
   // Mapping the state from the Provider.
   const mappedState = useSelector((state) => {
-    console.log('state from useSelector', state);
+    console.log(LOG, 'state from useSelector', state);
     return state;
   });
-  console.log('state from mappedState', mappedState);
+
+  // Mapping the dispatch function.
+  const mappedDispatch = useDispatch();
+
+  // Getting the ID from the last item in the list.
+  const lastToDoId = mappedState.toDoList[mappedState.toDoList.length - 1].id;
+
+  // Creating the new To Do item, if the user clicks "Add To Do".
+  const newToDoId = lastToDoId + 1;
+  const newToDo = {
+    id: newToDoId,
+    descricao: 'New To Do',
+    key: newToDoId.toString(),
+  };
+
+  /** Add a new To Do. */
+  const addToDo = () => {
+    mappedDispatch({ type: ADD_TO_DO, toDo: newToDo });
+  };
+
+  /** Removes the last To Do of the list. */
+  const removeToDo = () => {
+    if (lastToDoId <= 1) {
+      console.log(LOG, 'emptying the list is not possible');
+    } else {
+      mappedDispatch({
+        type: REMOVE_TO_DO,
+        toDoId: lastToDoId,
+      });
+    }
+  };
 
   return (
     <>
@@ -25,6 +59,9 @@ export const ToDoListComponent = () => {
         data={mappedState.toDoList}
         renderItem={({ item }) => <ToDoItem item={item} />}
       />
+
+      <Button title="Add To Do" onPress={() => addToDo()} />
+      <Button title="Remove Last To Do" onPress={() => removeToDo()} />
     </>
   );
 };
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,26 +1,34 @@
 import { ADD_TO_DO, REMOVE_TO_DO } from './actions';
 
+const LOG = '[TO DO REDUCER]';
+
 const initialState = {
-  toDoList: [{ id: 1, descricao: 'Meu primeiro To Do...', key: '1' }],
+  toDoList: [{ id: 1, descricao: 'My first To Do...', key: '1' }],
 };
 
 export const toDoReducer = (state = initialState, action) => {
+  console.log(LOG, state, action);
   // Load the current To Do list.
   let toDo = [...state.toDoList];
+  console.log(LOG, 'toDo before', toDo);
 
   // Checking which Action to execute.
   switch (action.type) {
     case ADD_TO_DO:
-      // Adding the object to the array without reference (cloning via spread operator).
-      toDo.push({ ...action.payload });
+      // Adding the object to the array.
+      toDo.push(action.toDo);
+      break;
 
     case REMOVE_TO_DO:
       // Finding the To Do index.
-      let index = toDo.findIndex((item) => item.id === action.payload);
+      let index = toDo.findIndex((item) => item.id === action.toDoId);
 
       // Removing the item from the array.
       toDo.splice(index, 1);
+      break;
   }
+
+  console.log(LOG, 'toDo after', toDo);
 
   // Returning the current state and overwriting the changed property.
   return { ...state, toDoList: toDo };
